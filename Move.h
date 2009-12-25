4c9418cad5eb4dcac8497972e427f4077702d159
Travis Wolfe <travis@Travis-Wolfes-MacBook-Pro.local>
2009-12-25 16:33:48 -0500

    added move header file

---
+++ Move.h
@@ -0,0 +1,19 @@
+/* Travis Wolfe
+ * December 2009
+ */
+
+#include <stdlib.h>
+#include "Board.h"
+
+/* Move needs to store from, to, capture type, promote type
+ * should have a bunch of methods associted with it
+ */
+typedef struct {
+	
+} Move;
+
+int moving_type();
+int capturing_type();
+int from();
+int to();
+
