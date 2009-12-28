d40ca1b789ec3d2a68d969f7042c251a3408738c
Travis Wolfe <travis@TW-MBP.local>
2009-12-27 19:52:20 -0500

    nothing important

--- Board.c
+++ Board.c
@@ -3,7 +3,6 @@
  */
 
 #include "Board.h"
-#include "Move.h"
 
 #define SQUARE(X) (1L << (X))
 #define IS_ZERO(BITS, INDEX) ((BITS & 1<<INDEX) == 0)
@@ -33,30 +32,30 @@ void initf(Board *board, char *fen) {
 		switch(c) {
 			
 			case 'K':
-				board->positions[WHITE*6 + KING] |= sq; break;
+				board->rank_positions[WHITE*6 + KING] |= sq; break;
 			case 'Q':
-				board->positions[WHITE*6 + QUEEN] |= sq; break;
+				board->rank_positions[WHITE*6 + QUEEN] |= sq; break;
 			case 'R':
-				board->positions[WHITE*6 + ROOK] |= sq; break;
+				board->rank_positions[WHITE*6 + ROOK] |= sq; break;
 			case 'B':
-				board->positions[WHITE*6 + BISHOP] |= sq; break;
+				board->rank_positions[WHITE*6 + BISHOP] |= sq; break;
 			case 'N':
-				board->positions[WHITE*6 + KNIGHT] |= sq; break;
+				board->rank_positions[WHITE*6 + KNIGHT] |= sq; break;
 			case 'P':
-				board->positions[WHITE*6 + PAWN] |= sq; break;
+				board->rank_positions[WHITE*6 + PAWN] |= sq; break;
 			
 			case 'k':
-				board->positions[BLACK*6 + KING] |= sq; break;
+				board->rank_positions[BLACK*6 + KING] |= sq; break;
 			case 'q':
-				board->positions[BLACK*6 + QUEEN] |= sq; break;
+				board->rank_positions[BLACK*6 + QUEEN] |= sq; break;
 			case 'r':
-				board->positions[BLACK*6 + ROOK] |= sq; break;
+				board->rank_positions[BLACK*6 + ROOK] |= sq; break;
 			case 'b':
-				board->positions[BLACK*6 + BISHOP] |= sq; break;
+				board->rank_positions[BLACK*6 + BISHOP] |= sq; break;
 			case 'n':
-				board->positions[BLACK*6 + KNIGHT] |= sq; break;
+				board->rank_positions[BLACK*6 + KNIGHT] |= sq; break;
 			case 'p':
-				board->positions[BLACK*6 + PAWN] |= sq; break;
+				board->rank_positions[BLACK*6 + PAWN] |= sq; break;
 				
 			/* new rank */
 			case '/':
@@ -107,30 +106,30 @@ void printb(Board *board) {
 			
 			sq = SQUARE(col*8+row);
 			
-			if(board->positions[WHITE*6+QUEEN] & sq)
+			if(board->rank_positions[WHITE*6+QUEEN] & sq)
 				b[row][col] = 'Q';
-			else if(board->positions[WHITE*6+KING] & sq)
+			else if(board->rank_positions[WHITE*6+KING] & sq)
 				b[row][col] = 'K';
-			else if(board->positions[WHITE*6+ROOK] & sq)
+			else if(board->rank_positions[WHITE*6+ROOK] & sq)
 				b[row][col] = 'R';
-			else if(board->positions[WHITE*6+BISHOP] & sq)
+			else if(board->rank_positions[WHITE*6+BISHOP] & sq)
 				b[row][col] = 'B';
-			else if(board->positions[WHITE*6+KNIGHT] & sq)
+			else if(board->rank_positions[WHITE*6+KNIGHT] & sq)
 				b[row][col] = 'N';
-			else if(board->positions[WHITE*6+PAWN] & sq)
+			else if(board->rank_positions[WHITE*6+PAWN] & sq)
 				b[row][col] = 'P';
 			
-			else if(board->positions[BLACK*6+QUEEN] & sq)
+			else if(board->rank_positions[BLACK*6+QUEEN] & sq)
 				b[row][col] = 'q';
-			else if(board->positions[BLACK*6+KING] & sq)
+			else if(board->rank_positions[BLACK*6+KING] & sq)
 				b[row][col] = 'k';
-			else if(board->positions[BLACK*6+ROOK] & sq)
+			else if(board->rank_positions[BLACK*6+ROOK] & sq)
 				b[row][col] = 'r';
-			else if(board->positions[BLACK*6+BISHOP] & sq)
+			else if(board->rank_positions[BLACK*6+BISHOP] & sq)
 				b[row][col] = 'b';
-			else if(board->positions[BLACK*6+KNIGHT] & sq)
+			else if(board->rank_positions[BLACK*6+KNIGHT] & sq)
 				b[row][col] = 'n';
-			else if(board->positions[BLACK*6+PAWN] & sq)
+			else if(board->rank_positions[BLACK*6+PAWN] & sq)
 				b[row][col] = 'p';
 			
 			else b[row][col] = '-';
@@ -154,30 +153,28 @@ void setup(Board *board) {
 	
 	set_play(board, WHITE);
 	
-	board->positions = (long*) malloc(12*sizeof(long));
-	
-	board->positions[WHITE*6+KING] = SQUARE(4);
-	board->positions[WHITE*6+QUEEN] = SQUARE(3);
-	board->positions[WHITE*6+ROOK] = SQUARE(0) | SQUARE(7);
-	board->positions[WHITE*6+BISHOP] = SQUARE(2) | SQUARE(5);
-	board->positions[WHITE*6+KNIGHT] = SQUARE(1) | SQUARE(6);
-	board->positions[WHITE*6+PAWN] = SQUARE(8) | SQUARE(9) | SQUARE(10) |
+	board->rank_positions[WHITE*6+KING] = SQUARE(4);
+	board->rank_positions[WHITE*6+QUEEN] = SQUARE(3);
+	board->rank_positions[WHITE*6+ROOK] = SQUARE(0) | SQUARE(7);
+	board->rank_positions[WHITE*6+BISHOP] = SQUARE(2) | SQUARE(5);
+	board->rank_positions[WHITE*6+KNIGHT] = SQUARE(1) | SQUARE(6);
+	board->rank_positions[WHITE*6+PAWN] = SQUARE(8) | SQUARE(9) | SQUARE(10) |
 									SQUARE(11) | SQUARE(12) | SQUARE(13) |
 									SQUARE(14) | SQUARE(15);
 	
-	board->positions[BLACK*6+KING] = SQUARE(60);
-	board->positions[BLACK*6+QUEEN] = SQUARE(59);
-	board->positions[BLACK*6+ROOK] = SQUARE(56) | SQUARE(63);
-	board->positions[BLACK*6+BISHOP] = SQUARE(58) | SQUARE(61);
-	board->positions[BLACK*6+KNIGHT] = SQUARE(57) | SQUARE(62);
-	board->positions[BLACK*6+PAWN] = SQUARE(48) | SQUARE(49) | SQUARE(50) |
+	board->rank_positions[BLACK*6+KING] = SQUARE(60);
+	board->rank_positions[BLACK*6+QUEEN] = SQUARE(59);
+	board->rank_positions[BLACK*6+ROOK] = SQUARE(56) | SQUARE(63);
+	board->rank_positions[BLACK*6+BISHOP] = SQUARE(58) | SQUARE(61);
+	board->rank_positions[BLACK*6+KNIGHT] = SQUARE(57) | SQUARE(62);
+	board->rank_positions[BLACK*6+PAWN] = SQUARE(48) | SQUARE(49) | SQUARE(50) |
 									SQUARE(51) | SQUARE(52) | SQUARE(53) |
 									SQUARE(54) | SQUARE(55);
 }
 
 void dispose(Board *board) {
 	printf("[Board.dispose]\t\tgetting rid of the board\n");
-	/* free(board->positions); */
+	/* free(board->rank_positions); */
 }
 
 int ply(Board *board) {
